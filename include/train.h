f40b8315099410e618f406d8e71efe10ee087ded
peshinam <frrr.emaeeeee@mail.ru>
2026-05-31 22:38:35 +0300

    Update train.h

--- include/train.h
+++ include/train.h
@@ -1,20 +1,29 @@
-// Copyright 2022 NNTU-CS
-#ifndef INCLUDE_TRAIN_H_
-#define INCLUDE_TRAIN_H_
+#ifndef TRAIN_H
+#define TRAIN_H
 
 class Train {
- private:
-  struct Car {
-    bool light; // состояние лампочки
-    Car *next;
-    Car *prev;
-  };
-  int countOp; // счетчик шагов (число переходов из вагона в вагон)
-  Car *first; // точка входа в поезд (первый вагон)
- public:
-  Train();
-  void addCar(bool light); // добавить вагон с начальным состоянием лампочки
-  int getLength();          // вычислить длину поезда
-  int getOpCount();         // вернуть число переходов (из вагона в вагон)
+private:
+    struct Car {
+        bool light;     // состояние лампочки
+        Car* next;      // указатель на следующий вагон
+        Car* prev;      // указатель на предыдущий вагон
+
+        Car(bool l) : light(l), next(nullptr), prev(nullptr) {}
+    };
+
+    int countOp;        // счетчик шагов (число переходов)
+    Car* first;         // точка входа в поезд (первый вагон)
+    int trainLength;    // реальная длина поезда (для эксперимента)
+
+public:
+    Train();
+    ~Train();
+
+    void addCar(bool light);    // добавить вагон
+    int getLength();             // вычислить длину поезда
+    int getOpCount();            // вернуть число переходов
+    void resetOpCount();         // сбросить счетчик операций
+    int getRealLength();         // получить реальную длину (для эксперимента)
 };
-#endif  // INCLUDE_TRAIN_H_
+
+#endif
